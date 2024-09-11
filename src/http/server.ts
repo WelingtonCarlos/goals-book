cd90eb8fb55bb3c2dd56a65a90fc397c819903e5
WelingtonCarlos <wcfilho98@hotmail.com>
2024-09-11 16:40:23 -0300

    second commit

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,12 +7,23 @@ import {
 } from "fastify-type-provider-zod";
 
 import { createGoal } from "../functions/create-goal";
-import { createGoalSchema } from "../utils/validations";
+import { createGoalCompletion } from "../functions/create-goals-completion";
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createCompletionSchema, createGoalSchema } from "../utils/validations";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+    const { pendingGoals } = await getWeekPendingGoals()
+    return { pendingGoals }
+})
+app.get('/every-goals', async () => {
+    const { everyGoals } = await getWeekPendingGoals()
+    return { everyGoals }
+})
 
 app.post('/goals',
     {
@@ -30,6 +41,18 @@ app.post('/goals',
 
         if (createdGoal) return createdGoal
     })
+app.post('/create-completion', {
+    schema: {
+        body: createCompletionSchema
+    }
+}, async (request) => {
+    const { goalId } = request.body
+
+    await createGoalCompletion({
+        goalId
+    })
+})
+
 
 app
     .listen({ port: 3333 })
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -8,3 +8,7 @@ export const createGoalSchema = z.object({
     title: z.string().min(2),
     desiredWeeklyFrequency: z.number().min(1).max(7)
 })
+
+export const createCompletionSchema = z.object({
+    goalId: z.string()
+})
